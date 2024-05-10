c1163f7dba6b48a3ab527201cc603d5e7c3eb228
SabinSnider <sabinvsic@gmail.com>
2024-05-10 11:14:49 +0545

    Add: style and call carousel component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
+import EmblaCarousel from "./components/EmblaCrousel";
+import { EmblaOptionsType } from "embla-carousel";
+
+const OPTIONS: EmblaOptionsType = { loop: true ,}
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    
+       <EmblaCarousel options={OPTIONS} />
+
     </main>
   );
 }
